121a0e5308a33991717f60b3d9573c2dfb3bab13
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-04-03 23:13:43 +0200

    Add function

---
+++ main.js
@@ -0,0 +1,9 @@
+const listItem = document.querySelector(".nav-list")
+const openMenu = document.querySelector(".nav")
+const closeMenu = document.querySelector(".close")
+const popUp = document.querySelector(".popup-menu")
+
+
+openMenu.addEventListener("click", () => {
+    popUp.style.display = "block"
+})
