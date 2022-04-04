aa92035e30c80891e77abd42197c515d229afe3f
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-04-04 20:48:30 +0200

    Update m-menu

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-const listItem = document.querySelector(".nav-list")
 const bars = document.getElementById('pop')
 const closeMenu = document.querySelector(".close")
 const popUp = document.querySelector(".menu-up")
 
 
-// listItem.addEventListener("click", () => {
-// listItem.style.display = "block"
-// })
+
 bars.addEventListener("click", () => {
-    console.log(12);
     popUp.classList.add('pop-up')
 })
 
-// closeMenu.addEventListener("click", () => {
-//     popUp.style.display = "none"
-// })
+popUp.addEventListener("click", () => {
+popUp.style.display ="none"
+})
+
+closeMenu.addEventListener("click", () => {
+    popUp.style.display = "none"
+})
