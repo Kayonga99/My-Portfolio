c2f9be6dab7ac39eb13f1e2ce6e55065a46f6d4b
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-04-11 09:36:44 +0200

    Update mobile-menu.

--- main.js
+++ main.js
@@ -1,19 +1,4 @@
-const bars = document.getElementById('pop');
-const closeMenu = document.querySelector('.close');
-const popUp = document.querySelector('.menu-up');
-
-bars.addEventListener('click', () => {
-  popUp.classList.add('pop-up');
-});
-
-closeMenu.addEventListener('click', () => {
-  popUp.classList.remove('pop-up');
-});
-
-popUp.addEventListener('click', () => {
-  popUp.classList.remove('pop-up');
-});
-
+// portfolio-mobile menu
 // cards
 const cardsUl = document.getElementById('cards');
 
